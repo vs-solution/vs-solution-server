0e190f123286b831447f95e3f017b66b6cb54727
longetka <longetka@gmail.com>
2021-04-21 21:30:45 +0700

    deploy app with cors

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const mongoose			= require('mongoose');
 const expressUpload		= require('express-fileupload');
 const app				= express();
 const PORT				= process.env.PORT || 5000;
+const cors				= require('cors');
 
+app.use(cors());
 app.use(express.json({extended: true}));
 app.use(expressUpload());
 app.use('/sell/', require('./routes/reqData'));
